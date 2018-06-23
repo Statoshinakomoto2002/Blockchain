4a4c0e8d98d354c25d98c5c6093b7fcfefe9a92e
hainv <hai213k57@gmail.com>
2018-06-23 22:33:22 +0700

    Create demo hello world with blockchain
    Signed-off-by: hainv <hai213k57@gmail.com>

--- examples/hello-world/README.md
+++ examples/hello-world/README.md
@@ -1,2 +1,5 @@
+## Example
+* Tạo một chuỗi khối mặc định và sau khi tạo sẽ không được quyền sửa đổi nội dung của một Block. Blockchain là một nền tảng lưu trữ dữ liệu phân tán, không đươc quản lý bởi một node cụ thể nào.
+
 ## Ref
 * https://codingislove.com/simple-blockchain-javascript/
--- examples/hello-world/main.js
+++ examples/hello-world/main.js
@@ -1,13 +1,16 @@
+const SHA256 = require('sha256');
+
 class Block {
     constructor(index, data, prevHash) {
         this.index = index;
+        this.timestamp = Math.floor(Date.now() / 1000);
         this.data = data;
         this.prevHash = prevHash;
-        this.timestamp = Math.floor(Date.now() / 1000);
+        this.hash = this.getHash();
     }
 
     getHash() {
-        return sha(JSON.stringify(this.data) + this.prevHash + this.index + this.timestamp);
+        return SHA256(JSON.stringify(this.data) + this.prevHash + this.index + this.timestamp);
     }
 }
 
@@ -15,4 +18,41 @@ class BlockChain {
     constructor() {
         this.chain = [];
     }
+
+    addBlock(data) {
+        let index = this.chain.length;
+        let prevHash = this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : 0;
+        let block = new Block(index, data, prevHash);
+        this.chain.push(block);
+    }
+
+    chainIsValid() {
+        for (let i = 0; i < this.chain.length; i++) {
+            if (this.chain[i].hash != this.chain[i].getHash()) {
+                console.log('Hash is\'t valid');
+                return false;
+            }
+            if (i > 0 && this.chain[i].prevHash != this.chain[i - 1].hash) {
+                console.log('Prev hash is\'t valid');
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
+// Demo CILCoin
+var CILCoin = new BlockChain();
+CILCoin.addBlock({ sender: "Bruce wayne", reciver: "Tony stark", amount: 100 });
+4
+CILCoin.addBlock({ sender: "Harrison wells", reciver: "Han solo", amount: 50 });
+5
+CILCoin.addBlock({ sender: "Tony stark", reciver: "Ned stark", amount: 75 });
+// console.log(JSON.stringify(CILCoin, null, 4));
+
+// Check chain
+// Step 1: True
+console.log("Validity: ", CILCoin.chainIsValid());
+// Setp 2: False
+CILCoin.chain[0].data.reciver = "Joker";
+console.log('Validity: ', CILCoin.chainIsValid());
